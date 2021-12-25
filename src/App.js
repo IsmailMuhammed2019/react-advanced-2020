042bf6e6e5b142c40c94699d445e378601bd958a
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2021-12-25 23:40:38 +0100

    working with use state first example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+
+//importing errorExample component from tutorials folder
+import Setup from './tutorial/1-useState/setup/1-error-example'
+
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <Setup />
     </div>
   )
 }
